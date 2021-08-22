c590d3cc76bb732a4b3563db425851c87c8e10de
Franlon <abdiel.franco@upr.edu>
2021-08-22 16:06:19 -0400

    Adding login / logout to rest of the screens

--- js/menu.js
+++ js/menu.js
@@ -62,3 +62,22 @@ function addMenuButton(){
 	
 	document.querySelector("[name=top-menu]").appendChild(menuListitem);
 }
+
+ $(document).ready(function() {
+            
+	$("#sidebar").mCustomScrollbar({
+		theme: "minimal"
+	});
+
+	$('#dismiss, .overlay').on('click', function() {
+		$('#sidebar').removeClass('active');
+		$('.overlay').removeClass('active');
+	});
+
+	$('#sidebarCollapse').on('click', function() {
+		$('#sidebar').addClass('active');
+		$('.overlay').addClass('active');
+		$('.collapse.in').toggleClass('in');
+		$('a[aria-expanded=true]').attr('aria-expanded', 'false');
+	});
+});
