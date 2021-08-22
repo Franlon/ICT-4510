f47efa1e904460b0cbeddd860699d7ba0d5dd657
Franlon <abdiel.franco@upr.edu>
2021-08-22 16:01:33 -0400

    Adding login / logout to About

--- js/index.js
+++ js/index.js
@@ -1,95 +1,26 @@
- 
- 	let user = JSON.parse(sessionStorage.getItem('user'));
- 
- 	console.log(user);
- 
- 	if (user === null) {
- 	
- 		let login = document.createTextNode("Login");
- 		
- 		let link = document.createElement("a");
- 		link.setAttribute('href', 'login.html');
- 		link.setAttribute('class', 'button');
- 		link.appendChild(login);
- 	
- 		let loginListItem = document.createElement("li");
- 		loginListItem.setAttribute('class', 'button_user');
- 		loginListItem.appendChild(link);
- 		
- 		document.querySelector("[name=top-menu]").appendChild(loginListItem);
- 		addMenuButton();
- 	
- 	}else{
- 	
- 		let text = "Welcome, ".concat(user.user.first_name);
- 		var welcome = document.createTextNode(text);
-		var welcomeListItem = document.createElement("li");
-		welcomeListItem.appendChild(welcome);
-		document.querySelector("[name=top-menu]").appendChild(welcomeListItem);
-	
-		let logout = document.createTextNode("Logout");
-		
-		let link = document.createElement("a");
- 		link.setAttribute('href', '#');
- 		link.setAttribute('class', 'button');
- 		link.appendChild(logout); 
-		
-		let logoutListItem = document.createElement("li");
-		logoutListItem.setAttribute('class', 'button_user');
-		logoutListItem.appendChild(link);
-		logoutListItem.addEventListener('click',function(event){                          
-        
-        	sessionStorage.removeItem('user');
-        	window.location.reload(true);
- 		}); 
-		
-		document.querySelector("[name=top-menu]").appendChild(logoutListItem);
-		
-		addMenuButton();
- 	}
- 
-function addMenuButton(){
 
-	let image = document.createElement("img");
-	image.setAttribute('src', 'images/menu_icon.png');
-		
-	let button = document.createElement("button");
-	button.setAttribute('type', 'button');
-	button.setAttribute('id', 'sidebarCollapse');
-	button.appendChild(image);
-	
-	let menuListitem = document.createElement("li");
-	menuListitem.appendChild(button);
-	
-	document.querySelector("[name=top-menu]").appendChild(menuListitem);
-}
- 
 $(document).ready(function() {
- 
 
- 
- 
-            $("#sidebar").mCustomScrollbar({
-                theme: "minimal"
-            });
+	$("#sidebar").mCustomScrollbar({
+		theme: "minimal"
+	});
 
-            $('#dismiss, .overlay').on('click', function() {
-                $('#sidebar').removeClass('active');
-                $('.overlay').removeClass('active');
-            });
+	$('#dismiss, .overlay').on('click', function() {
+		$('#sidebar').removeClass('active');
+		$('.overlay').removeClass('active');
+	});
 
-            $('#sidebarCollapse').on('click', function() {
-                $('#sidebar').addClass('active');
-                $('.overlay').addClass('active');
-                $('.collapse.in').toggleClass('in');
-                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-            });
-        });
+	$('#sidebarCollapse').on('click', function() {
+		$('#sidebar').addClass('active');
+		$('.overlay').addClass('active');
+		$('.collapse.in').toggleClass('in');
+		$('a[aria-expanded=true]').attr('aria-expanded', 'false');
+	});
+});
         
-        
-        
-         $(document).ready(function() {
-           var owl = $('.owl-carousel');
+$(document).ready(function() {
+
+	var owl = $('.owl-carousel');
            owl.owlCarousel({
              margin: 10,
              nav: true,
