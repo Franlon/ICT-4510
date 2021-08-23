3a2a1ab3d1d087deed367aeb997766eb3d48938c
Franlon <abdiel.franco@upr.edu>
2021-08-22 22:09:10 -0400

    Formatting

--- js/menu.js
+++ js/menu.js
@@ -1,52 +1,57 @@
- 	let user = JSON.parse(sessionStorage.getItem('user'));
- 
- 	console.log(user);
- 
- 	if (user === null) {
- 	
- 		let login = document.createTextNode("Login");
- 		
- 		let link = document.createElement("a");
- 		link.setAttribute('href', 'login.html');
- 		link.setAttribute('class', 'button');
- 		link.appendChild(login);
- 	
- 		let loginListItem = document.createElement("li");
- 		loginListItem.setAttribute('class', 'button_user');
- 		loginListItem.appendChild(link);
- 		
- 		document.querySelector("[name=top-menu]").appendChild(loginListItem);
- 		addMenuButton();
- 	
- 	}else{
- 	
- 		let text = "Welcome, ".concat(user.user.first_name);
- 		var welcome = document.createTextNode(text);
-		var welcomeListItem = document.createElement("li");
-		welcomeListItem.appendChild(welcome);
-		document.querySelector("[name=top-menu]").appendChild(welcomeListItem);
+/*
+   Abdiel L. Franco
+   ICT-4510
+   Summer 2021
+   Shared script by all the webpages. It updates the different menu items based on the user logged in status.
+*/
+
+let user = JSON.parse(sessionStorage.getItem('user'));
+
+if (user === null) {
+
+	let login = document.createTextNode("Login");
 	
-		let logout = document.createTextNode("Logout");
-		
-		let link = document.createElement("a");
- 		link.setAttribute('href', '#');
- 		link.setAttribute('class', 'button');
- 		link.appendChild(logout); 
-		
-		let logoutListItem = document.createElement("li");
-		logoutListItem.setAttribute('class', 'button_user');
-		logoutListItem.appendChild(link);
-		logoutListItem.addEventListener('click',function(event){                          
-        
-        	sessionStorage.removeItem('user');
-        	window.location.reload(true);
- 		}); 
-		
-		document.querySelector("[name=top-menu]").appendChild(logoutListItem);
-		
-		addMenuButton();
-		addSidebarItems();
- 	}
+	let link = document.createElement("a");
+	link.setAttribute('href', 'login.html');
+	link.setAttribute('class', 'button');
+	link.appendChild(login);
+
+	let loginListItem = document.createElement("li");
+	loginListItem.setAttribute('class', 'button_user');
+	loginListItem.appendChild(link);
+	
+	document.querySelector("[name=top-menu]").appendChild(loginListItem);
+	addMenuButton();
+
+}else{
+
+	let text = "Welcome, ".concat(user.user.first_name);
+	var welcome = document.createTextNode(text);
+	var welcomeListItem = document.createElement("li");
+	welcomeListItem.appendChild(welcome);
+	document.querySelector("[name=top-menu]").appendChild(welcomeListItem);
+
+	let logout = document.createTextNode("Logout");
+	
+	let link = document.createElement("a");
+	link.setAttribute('href', '#');
+	link.setAttribute('class', 'button');
+	link.appendChild(logout); 
+	
+	let logoutListItem = document.createElement("li");
+	logoutListItem.setAttribute('class', 'button_user');
+	logoutListItem.appendChild(link);
+	logoutListItem.addEventListener('click',function(event){                          
+	
+		sessionStorage.removeItem('user');
+		window.location.reload(true);
+	}); 
+	
+	document.querySelector("[name=top-menu]").appendChild(logoutListItem);
+	
+	addMenuButton();
+	addSidebarItems();
+}
  
 function addMenuButton(){
 
