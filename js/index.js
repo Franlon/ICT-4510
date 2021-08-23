89af557900ef3bfdbb0b2eb447c4ad89a0191721
Franlon <abdiel.franco@upr.edu>
2021-08-22 21:45:07 -0400

    Formatting Home screen related files

--- js/index.js
+++ js/index.js
@@ -1,21 +1,27 @@
+/*
+   Abdiel L. Franco
+   ICT-4510
+   August 22, 2021
+   Assignment Week #10: Moved this carousel script from the template to its own file
+*/
   
 $(document).ready(function() {
 
 	var owl = $('.owl-carousel');
-           owl.owlCarousel({
-             margin: 10,
-             nav: true,
-             loop: true,
-             responsive: {
-               0: {
-                 items: 1
-               },
-               600: {
-                 items: 2
-               },
-               1000: {
-                 items: 5
-               }
-             }
-           })
-         })
+	owl.owlCarousel({
+    	margin: 10,
+        nav: true,
+        loop: true,
+    	responsive: {
+        	0: {
+            	items: 1
+            },
+            600: {
+            	items: 2
+            },
+            1000: {
+            	items: 5
+            }
+        }
+    })
+})
